28b4b5a124f71f69086d736b1741d8845e550cbb
revalinda <revalindaph@gmail.com>
2026-02-20 22:14:07 +0700

    Percobaan 3: Membuat Konstruktor

--- Jobsheet2Object/Mahasiswa21.java
+++ Jobsheet2Object/Mahasiswa21.java
@@ -5,6 +5,17 @@ public class Mahasiswa21 {
     String nama, nim, kelas;
     Double ipk;
 
+    public Mahasiswa21() {
+
+    }
+
+    public Mahasiswa21(String nm, String nim, double ipk, String kls) {
+        nama = nm;
+        this.nim = nim;
+        this.ipk=ipk;
+        kelas = kls;
+    }
+
     void tampilkanInformasi() {
         System.out.println("Nama: " + nama);
         System.out.println("NIM: " + nim);
@@ -47,31 +58,3 @@ public class Mahasiswa21 {
         System.out.println(mhs.nilaiKinerja());
     }
 }
-
-/* Pertanyaan Percobaan 1: Deklarasi Class, Atribut dan Method
-
-1. Sebutkan dua karakteristik class atau object!
-    Memiliki identitas (identity), setiap object bisa dibedakan satu sama lain.
-    Memiliki atribut dan method, sebagai penyimpan data dan aksi.
-
-2. Perhatikan class Mahasiswa pada Praktikum 1 tersebut, ada berapa atribut yang dimiliki oleh class
-Mahasiswa? Sebutkan apa saja atributnya! Ada 4 yaitu, nama,nim,kelas,dan ipk.
-
-3. Ada berapa method yang dimiliki oleh class tersebut? Sebutkan apa saja methodnya! Ada 4 method, yaitu:
-tampilkanInformasi(), ubahKelas(String kelasBaru), updateIPK(Double ipkBaru), nilaiKinerja().
-
-4. Perhatikan method updateIpk() yang terdapat di dalam class Mahasiswa. Modifikasi isi method
-tersebut sehingga IPK yang dimasukkan valid yaitu terlebih dahulu dilakukan pengecekan apakah
-IPK yang dimasukkan di dalam rentang 0.0 sampai dengan 4.0 (0.0 <= IPK <= 4.0). Jika IPK tidak
-pada rentang tersebut maka dikeluarkan pesan: "IPK tidak valid. Harus antara 0.0 dan 4.0".
-
-5. Jelaskan bagaimana cara kerja method nilaiKinerja() dalam mengevaluasi kinerja mahasiswa,
-kriteria apa saja yang digunakan untuk menentukan nilai kinerja tersebut, dan apa yang
-dikembalikan (di-return-kan) oleh method nilaiKinerja() tersebut?
-    Untuk mengechek kriteria ipk nya. Kriteria yang digunakan yaitu, jika IPK ≥ 3.5 maka outputnya "Kinerja sangat baik",
-    jika IPK ≥ 3.0 outputnya "Kinerja Baik", IPK ≥ 2.0 outputnya "Kinerja cukup", selain itu maka outputnya "Kinerja kurang".
-    Yang direturn kan adalah kategori Mahasiswa dengan tipe data String.
-
-6. Commit dan push kode program ke Github. Done
-
-*/
--- Jobsheet2Object/MahasiswaMain21.java
+++ Jobsheet2Object/MahasiswaMain21.java
@@ -9,24 +9,54 @@ public class MahasiswaMain21 {
         mhs1.ipk = 3.55;
 
         mhs1.tampilkanInformasi();
-        System.out.println(mhs1.nilaiKinerja());
+        mhs1.ubahKelas("SI 2K");
+        mhs1.updateIPK(3.60);
+        mhs1.tampilkanInformasi();
+    
+        Mahasiswa21 mhs2 = new Mahasiswa21("Annisa Nabila", "2141720106", 3.25, "TI 2L");
+        mhs2.updateIPK(3.30);
+        mhs2.tampilkanInformasi();
+
+        Mahasiswa21 mhsRevalinda = new Mahasiswa21("Revalinda Putri", "254107060053", 3.63, "SIB-1E");
+        mhsRevalinda.tampilkanInformasi();
+        System.out.println(mhsRevalinda.nilaiKinerja());
     }
 }
 
-/* Pertanyaan Percobaan 2: Instansiasi Object, serta Mengakses Atribut dan Method
+/* Pertanyaan Percobaan 3: Membuat Konstruktor
 
-1. Pada class MahasiswaMain, tunjukkan baris kode program yang digunakan untuk proses
-instansiasi! Apa nama object yang dihasilkan?
-    Mahasiswa21 mhs1 = new Mahasiswa21();
-    nama object yang dihasilkan yaitu mhs1.
+1. Pada class Mahasiswa di Percobaan 3, tunjukkan baris kode program yang digunakan untuk
+mendeklarasikan konstruktor berparameter!
+        public Mahasiswa21(String nm, String nim, double ipk, String kls) {
+        nama = nm;
+        this.nim = nim;
+        this.ipk = ipk;
+        kelas = kls;
+    }
 
-2. Bagaimana cara mengakses atribut dan method dari suatu objek? caranya dengan memberi titik (.),
-    antara nama object (mhs1) dan nama atribut (nama), jadinya mhs1.nama misal akan memanggil nama
-    Ali, maka nanti di ketik mhs1.nama = Ali, jadinya mengambil object nama, dimasukkan di atribut
-    nama lalu di isikan dengan Ali.
+2. Perhatikan class MahasiswaMain. Apa sebenarnya yang dilakukan pada baris program
+berikut? Mahasiswa21 mhs2 = new Mahasiswa21("Annisa Nabila", "2141720106", 3.25, "TI 2L");
+    untuk membuat object baru bernama mhs2 menggunakan konstruktor berparameter, disini mengisikan nilai:
+    nama = "Annisa Nabila"
+    nim = "2141720106"
+    ipk = 3.25
+    kelas = "TI 2L"
+    jadi nantinya akan langsung menginisialisasi data saat object dibuat.
 
-3. Mengapa hasil output pemanggilan method tampilkanInformasi() pertama dan kedua berbeda? karena 
-    perubahan data yang di dalam object yan berubah dimana awalnya 3.9 menjadi 3.55
-    Mahasiswa21 menjadi blueprint yang mana terdapat object yang bisa mengisikan data baru
-    pada atribut. Makannya outputnya menjadi beda.
+3. Hapus konstruktor default pada class Mahasiswa, kemudian compile dan run program.
+Bagaimana hasilnya? Jelaskan mengapa hasilnya demikian!
+    Hasilnya akan error, karena java ngga menemukan konstruktor tanpa parameter lagi, pada java
+    jika kita membut konstruktor berparameter , java tidak otomatis membuat konstruktor kosong,
+    jadinya program ngga bisa di compile.
+
+4. Setelah melakukan instansiasi object, apakah method di dalam class Mahasiswa harus diakses
+secara berurutan? Jelaskan alasannya!
+    Tidak harus, karena method dipanggil sesuai kebutuhan saja, bukan untuk urutan tertentu.
+    Selama object sudah dibuat, method bisa dipanggil kapan saja.
+
+5. Buat object baru dengan nama mhs<NamaMahasiswa> menggunakan konstruktor berparameter dari class Mahasiswa!
+    Done.
+
+6. Commit dan push kode program ke Github. Done
+
 */
