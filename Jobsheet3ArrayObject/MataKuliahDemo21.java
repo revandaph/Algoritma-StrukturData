633c04c0d428bd4bb09825a866a8ff0da7f2064b
revalinda <revalindaph@gmail.com>
2026-02-24 12:06:43 +0700

    Ralat Percobaan 3: Modif Pertanyaan 2

--- Jobsheet3ArrayObject/MataKuliahDemo21.java
+++ Jobsheet3ArrayObject/MataKuliahDemo21.java
@@ -1,19 +1,21 @@
 package Jobsheet3ArrayObject;
-
 import java.util.Scanner;
-
 public class MataKuliahDemo21 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         MataKuliahh21[] arrayOfMatakuliah = new MataKuliahh21[3];
+        String kode, nama, dummy;
+        int sks, jumlahJam;
 
         for (int i = 0; i < 3; i++) {
-            arrayOfMatakuliah[i] = new MataKuliahh21();
+            System.out.println("Masukkan Data Matakuliah ke-" + (i + 1));
             arrayOfMatakuliah[i].tambahData();
+            System.out.println("-------------------------------------");
         }
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < arrayOfMatakuliah.length; i++) {
             System.out.println("Data Matakuliah ke-" + (i + 1));
-            arrayOfMatakuliah[i].cetakInfo();
         }
+
     }
 }
--- Jobsheet3ArrayObject/MataKuliahh21.java
+++ Jobsheet3ArrayObject/MataKuliahh21.java
@@ -7,15 +7,12 @@ public class MataKuliahh21 {
     public String nama;
     public int sks;
     public int jumlahJam;
-
+    
     public MataKuliahh21(String kode, String nama, int sks, int jumlahJam) {
         this.kode = kode;
         this.nama = nama;
         this.sks = sks;
         this.jumlahJam = jumlahJam;
-    }
-
-    public MataKuliahh21() {
     }
 
     public void tambahData() {
@@ -28,14 +25,6 @@ public class MataKuliahh21 {
         sks = Integer.parseInt(sc.nextLine());
         System.out.print("Jumlah jam  : ");
         jumlahJam = Integer.parseInt(sc.nextLine());
-        System.out.println("-------------------------------");
-    }
-
-    public void cetakInfo() {
-        System.out.println("Kode       : " + kode);
-        System.out.println("Nama       : " + nama);
-        System.out.println("Sks        : " + sks);
-        System.out.println("Jumlah Jam : " + jumlahJam);
         System.out.println("-------------------------------");
     }
 }
