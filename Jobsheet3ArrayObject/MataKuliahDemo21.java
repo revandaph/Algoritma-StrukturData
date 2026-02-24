4e69eb930539bae521a72705431e31505f904e7d
revalinda <revalindaph@gmail.com>
2026-02-24 12:19:58 +0700

    Percobaan 3: Modif Pertanyaan 4

--- Jobsheet3ArrayObject/MataKuliahDemo21.java
+++ Jobsheet3ArrayObject/MataKuliahDemo21.java
@@ -1,19 +1,22 @@
 package Jobsheet3ArrayObject;
+
 import java.util.Scanner;
+
 public class MataKuliahDemo21 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        MataKuliahh21[] arrayOfMatakuliah = new MataKuliahh21[3];
-        String kode, nama, dummy;
-        int sks, jumlahJam;
+        System.out.print("Masukkan jumlah matakuliah: ");
+        int n = Integer.parseInt(sc.nextLine());
+        
+        MataKuliahh21[] arrayOfMatakuliah = new MataKuliahh21[n];
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < n; i++) {
+            arrayOfMatakuliah[i] = new MataKuliahh21();
             System.out.println("Masukkan Data Matakuliah ke-" + (i + 1));
             arrayOfMatakuliah[i].tambahData();
-            System.out.println("-------------------------------------");
         }
         
-        for (int i = 0; i < arrayOfMatakuliah.length; i++) {
+        for (int i = 0; i < n; i++) {
             System.out.println("Data Matakuliah ke-" + (i + 1));
             arrayOfMatakuliah[i].cetakInfo();
         }
--- Jobsheet3ArrayObject/MataKuliahh21.java
+++ Jobsheet3ArrayObject/MataKuliahh21.java
@@ -7,12 +7,15 @@ public class MataKuliahh21 {
     public String nama;
     public int sks;
     public int jumlahJam;
-    
+
     public MataKuliahh21(String kode, String nama, int sks, int jumlahJam) {
         this.kode = kode;
         this.nama = nama;
         this.sks = sks;
         this.jumlahJam = jumlahJam;
+    }
+
+    public MataKuliahh21() {
     }
 
     public void tambahData() {
