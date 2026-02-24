236e1bf539aef2da6f5e60c3d11ba1020f209ffd
revalinda <revalindaph@gmail.com>
2026-02-24 10:31:51 +0700

    Percobaan 2 : Modif Pertanyaan 1

--- Jobsheet3ArrayObject/MahasiswaDemo21.java
+++ Jobsheet3ArrayObject/MahasiswaDemo21.java
@@ -6,7 +6,6 @@ public class MahasiswaDemo21 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Mahasiswaa21[] arrayOfMahasiswaa21 = new Mahasiswaa21[3];
-        String dummy;
 
         for (int i = 0; i < 3; i++) {
             arrayOfMahasiswaa21[i] = new Mahasiswaa21();
@@ -19,19 +18,13 @@ public class MahasiswaDemo21 {
             System.out.print("Kelas : ");
             arrayOfMahasiswaa21[i].kelas = sc.nextLine();
             System.out.print("IPK : ");
-            dummy = sc.nextLine();
-            arrayOfMahasiswaa21[i].ipk = Float.parseFloat(dummy);
+            arrayOfMahasiswaa21[i].ipk = Float.parseFloat(sc.nextLine());
             System.out.println("-------------------------------");
         }
         
         for (int i = 0; i < 3; i++) {
             System.out.println("Data Mahasiswa ke-" + (i + 1));
-            System.out.println("NIM : " + arrayOfMahasiswaa21[i].nim);
-            System.out.println("Nama : " + arrayOfMahasiswaa21[i].nama);
-            System.out.println("Kelas : " + arrayOfMahasiswaa21[i].kelas);
-            System.out.println("IPK : " + arrayOfMahasiswaa21[i].ipk);
-            System.out.println("-------------------------------");
+            arrayOfMahasiswaa21[i].cetakInfo();
         }
-
     }
 }
