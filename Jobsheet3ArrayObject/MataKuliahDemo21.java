f8545c0a3d621ff831ed59f377d132414594a917
revalinda <revalindaph@gmail.com>
2026-02-24 11:02:56 +0700

    Percobaan 3: Constructor Berparameter

---
+++ Jobsheet3ArrayObject/MataKuliahDemo21.java
@@ -0,0 +1,37 @@
+package Jobsheet3ArrayObject;
+
+import java.util.Scanner;
+
+public class MataKuliahDemo21 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        MataKuliahh21[] arrayOfMatakuliah = new MataKuliahh21[3];
+        String kode, nama, dummy;
+        int sks, jumlahJam;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Masukkan Data Matakuliah ke-" + (i + 1));
+            System.out.print("Kode        : ");
+            kode = sc.nextLine();
+            System.out.print("Nama        : ");
+            nama = sc.nextLine();
+            System.out.print("Sks         : ");
+            dummy = sc.nextLine();
+            sks = Integer.parseInt(dummy);
+            System.out.print("Jumlah jam  : ");
+            jumlahJam = Integer.parseInt(dummy);
+            System.out.println("---------------------------------");
+
+            arrayOfMatakuliah[i] = new MataKuliahh21(kode, nama, sks, jumlahJam);
+        }
+
+        for (int i = 0; i < arrayOfMatakuliah.length; i++) {
+            System.out.println("Data Matakuliah ke-" + (i + 1));
+            System.out.println("Kode        : " + arrayOfMatakuliah[i].kode);
+            System.out.println("Nama        : " + arrayOfMatakuliah[i].nama);
+            System.out.println("Sks         : " + arrayOfMatakuliah[i].sks);
+            System.out.println("Jumlah Jam  : " + arrayOfMatakuliah[i].jumlahJam);
+            System.out.println("-----------------------------------------------");
+        }
+    }
+}
---
+++ Jobsheet3ArrayObject/MataKuliahh21.java
@@ -0,0 +1,15 @@
+package Jobsheet3ArrayObject;
+
+public class MataKuliahh21 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahJam;
+    
+    public MataKuliahh21(String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+}
