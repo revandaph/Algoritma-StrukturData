117981f398c0b914ca1982488dbdd635866294e9
revalinda <revalindaph@gmail.com>
2026-03-31 09:33:13 +0700

    Praktikum 1C Sorting – Insertion Sort

--- Jobsheet6Sorting/SortingMain21.java
+++ Jobsheet6Sorting/SortingMain21.java
@@ -19,5 +19,14 @@ public class SortingMain21 {
         dataurut2.SelectionSort();
         System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
         dataurut2.tampil();
+
+        int c[] = {40, 10, 4, 9, 3};
+        Sorting21 dataurut3 = new Sorting21(c, c.length);
+
+        System.out.println("Data awal 3");
+        dataurut3.tampil();
+        dataurut3.insertionSort();
+        System.out.println("Data sudah diurutkan dengan INSERTION SORT (ASC)");
+        dataurut3.tampil();
     }
 }
