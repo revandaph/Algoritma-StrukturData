f653a431a352c8af577d4259cf0b36fc269dd40f
revalinda <revalindaph@gmail.com>
2026-03-31 12:38:52 +0700

    Modifikasi Praktikum 2C Mengurutkan Data Mahasiswa Berdasarkan IPK (Insertion Sort)

--- Jobsheet6Sorting/MahasiswaBerprestasi21.java
+++ Jobsheet6Sorting/MahasiswaBerprestasi21.java
@@ -51,7 +51,7 @@ public class MahasiswaBerprestasi21 {
             Mahasiswa21 temp = listMhs[i];
             int j = i;
 
-            while (j > 0 && listMhs[j - 1].ipk > temp.ipk) {
+            while (j > 0 && listMhs[j - 1].ipk < temp.ipk) {
                 listMhs[j] = listMhs[j - 1];
                 j--;
             }
