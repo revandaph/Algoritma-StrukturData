952626a28212fb82533e647e7c2302b190b85500
revalinda <revalindaph@gmail.com>
2026-03-31 11:31:31 +0700

    Modifikasi Praktikum 2A

--- Jobsheet6Sorting/MahasiswaDemo21.java
+++ Jobsheet6Sorting/MahasiswaDemo21.java
@@ -1,25 +1,38 @@
 package Jobsheet6Sorting;
 
+import java.util.Scanner;
+
 public class MahasiswaDemo21 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         MahasiswaBerprestasi21 list = new MahasiswaBerprestasi21();
-        Mahasiswa21 m1 = new Mahasiswa21("123", "Zidan", "2A", 3.2);
-        Mahasiswa21 m2 = new Mahasiswa21("124", "Ayu", "2A", 3.5);
-        Mahasiswa21 m3 = new Mahasiswa21("125", "Sofi", "2A", 3.1);
-        Mahasiswa21 m4 = new Mahasiswa21("126", "Sita", "2A", 3.9);
-        Mahasiswa21 m5 = new Mahasiswa21("127", "Miki", "2A", 3.7);
 
-        list.tambah(m1);
-        list.tambah(m2);
-        list.tambah(m3);
-        list.tambah(m4);
-        list.tambah(m5);
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Data mahasiswa ke-" + (i+1));
 
-        System.out.println("Data mahasiswa sebelum sorting: ");
+            System.out.print("NIM: ");
+            String nim = sc.nextLine();
+
+            System.out.print("Nama: ");
+            String nama = sc.nextLine();
+
+            System.out.print("Kelas: ");
+            String kelas = sc.nextLine();
+
+            System.out.print("IPK: ");
+            String ipkStr = sc.nextLine();
+            double ipk = Double.parseDouble(ipkStr);
+
+            Mahasiswa21 m = new Mahasiswa21(nim, nama, kelas, ipk);
+            list.tambah(m);
+        }
+
+        System.out.println("\nData sebelum sorting:");
         list.tampil();
 
-        System.out.println("\nData Mahasiswa setelah sorting berdasarkan IPK (DESC) : ");
         list.bubbleSort();
+
+        System.out.println("\nData setelah sorting (DESC):");
         list.tampil();
     }
 }
