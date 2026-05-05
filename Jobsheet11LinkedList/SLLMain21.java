c32dc1b006a9134f1931942e010c59c9d649f451
revalinda <revalindaph@gmail.com>
2026-05-05 13:27:46 +0700

    Jobsheet 11 Linked List Praktikum 2.1 Modifikasi Soal No.3

--- Jobsheet11LinkedList/SLLMain21.java
+++ Jobsheet11LinkedList/SLLMain21.java
@@ -1,9 +1,12 @@
 package Jobsheet11LinkedList;
 
+import java.util.Scanner;
+
 public class SLLMain21 {
     public static void main(String[] args) {
 
         SingleLinkedList21 sll = new SingleLinkedList21();
+        Scanner sc = new Scanner(System.in);
 
         Mahasiswa21 mhs1 = new Mahasiswa21("24212200", "Alvaro", "1A", 4.0);
         Mahasiswa21 mhs2 = new Mahasiswa21("22212202", "Cintia", "3C", 3.5);
@@ -22,5 +25,26 @@ public class SLLMain21 {
 
         sll.insertAt(2, mhs2);
         sll.print();
+
+        System.out.println("=== Tambah Mahasiswa dari Keyboard ===");
+        System.out.print("Masukkan NIM   : ");
+        String nim = sc.nextLine();
+
+        System.out.print("Masukkan Nama  : ");
+        String nama = sc.nextLine();
+
+        System.out.print("Masukkan Kelas : ");
+        String kelas = sc.nextLine();
+
+        System.out.print("Masukkan IPK   : ");
+        double ipk = Double.parseDouble(sc.nextLine());
+
+        Mahasiswa21 mhsBaru = new Mahasiswa21(nim, nama, kelas, ipk);
+        sll.addLast(mhsBaru);
+
+        System.out.println("\nSetelah penambahan dari keyboard:");
+        sll.print();
+
+        sc.close();
     }
 }
